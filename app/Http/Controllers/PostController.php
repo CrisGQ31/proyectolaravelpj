5cc8233175ca8e88cfe27d6020431278c5a89282
CrisGQ31 <garciacristhian978@gmail.com>
2025-04-07 16:06:44 -0600

    Controladores 0704

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    private $post = [];
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => $this->post
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+}
